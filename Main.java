2b13a4e535d65447218a7a542b43de498c0198fb
jikolabo <jilaboscientist@gmail.com>
2021-06-06 22:44:22 +0900

    test

--- Main.java
+++ Main.java
@@ -1,16 +1,10 @@
+import java.io.*;
 
 public class Main {
-  public static void main(String[] args){
-    Bank b = new Bank();
-    Bank b2 = new Bank();
-    b.name = "みずほ";
-    b.address = "日本";
-    b2.name = "みずほ";
-    b2.address = "日本";
-
-    if(b.equals(b2)){
-      System.out.println("二つの銀行は同じ内容です。");
-    }
-
+  public static void main(String[] args) throws IOException{
+    FileOutputStream fos = new FileOutputStream("test2.dat", true);
+    fos.write(65);
+    fos.flush();
+    fos.close();
   }
 }
